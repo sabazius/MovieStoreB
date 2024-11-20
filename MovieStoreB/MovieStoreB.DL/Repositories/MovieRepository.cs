75bd36c4ca74f4d004e73dec6009c1f7dca0c62a
Boris Serafimov <b.b.serafimov@gmail.com>
2024-11-20 19:06:57 +0200

    movie_crud

--- MovieStoreB/MovieStoreB.DL/Repositories/MovieRepository.cs
+++ MovieStoreB/MovieStoreB.DL/Repositories/MovieRepository.cs
@@ -4,11 +4,34 @@ using MovieStoreB.Models.DTO;
 
 namespace MovieStoreB.DL.Repositories
 {
-    public class MovieRepository : IMovieRepository
+    internal class MovieRepository : IMovieRepository
     {
         public List<Movie> GetMovies()
         {
             return StaticData.Movies;
+        }
+
+        public void AddMovie(Movie movie)
+        {
+            StaticData.Movies.Add(movie);
+        }
+        public void DeleteMovie(int id)
+        {
+            if (id <= 0) return;
+
+            var movie = GetMoviesById(id);
+
+            if (movie != null)
+            {
+                StaticData.Movies.Remove(movie);
+            }
+        }
+        public Movie? GetMoviesById(int id)
+        {
+            if (id <= 0) return null;
+
+            return StaticData.Movies
+                .FirstOrDefault(x => x.Id == id);
         }
     }
 }
